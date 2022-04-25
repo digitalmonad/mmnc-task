2fec6c5959f640fe45ca3134145ff02868d81e14
Pavel Kocman <kocmanpavel@gmail.com>
2022-04-25 17:35:47 +0200

    chore: Setup React Query + add Storybook

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,21 @@
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'theme-ui'
+import { QueryClient, QueryClientProvider, Hydrate } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+import { useState } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [client] = useState(new QueryClient())
+
   return (
-    <ThemeProvider theme={{}}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <QueryClientProvider {...{ client }}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <ThemeProvider theme={{}}>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </Hydrate>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
 
