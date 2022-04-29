4bff64f57f3c930502f2f547186f417c0966b94a
Pavel Kocman <kocmanpavel@gmail.com>
2022-04-29 21:29:31 +0200

    fix: Remove styled-components module

---
+++ styles/globals.tsx
@@ -0,0 +1,34 @@
+import styled from '@emotion/styled'
+
+import { Global, css } from '@emotion/react'
+
+export const GlobalStyles = () => (
+  <Global
+    styles={css`
+      html,
+      body {
+        padding: 0;
+        margin: 0;
+        font-family: 'Epilogue', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell',
+          'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+      }
+
+      a {
+        color: inherit;
+        text-decoration: none;
+      }
+
+      * {
+        box-sizing: border-box;
+      }
+    `}
+  />
+)
+
+export const AppWrapper = styled('div')`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`
