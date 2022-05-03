066f0300ef4928dcab151b836cae37f6bfece88f
Pavel Kocman <kocmanpavel@gmail.com>
2022-05-03 17:50:14 +0200

    chore: Add i18n

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+const { i18n } = await require('./next-i18next.config')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n,
 }
 
 module.exports = nextConfig
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,20 @@
-import type { NextPage } from 'next'
+import type { NextPage, GetStaticProps } from 'next'
 import { dehydrate, QueryClient } from 'react-query'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+
 import { fetchCurrencies } from '../utils/currencies/fetchCurrencies'
 
 const Home: NextPage = ({}) => {
   return <div></div>
 }
 
-export const getStaticProps = async () => {
+export const getStaticProps: GetStaticProps = async (ctx) => {
   const client = new QueryClient()
   await client.prefetchQuery('currencies', fetchCurrencies)
 
   return {
     props: {
+      ...(await serverSideTranslations(ctx.locale as string, ['common'])),
       dehydratedState: dehydrate(client),
     },
   }
